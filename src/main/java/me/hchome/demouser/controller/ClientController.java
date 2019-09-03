1becfaaefa0b8fdd8585437294677b7d7303171d
cliffordpan.helenchen@gmail.com <Nhpjj811025>
2019-09-03 09:19:44 -0700

    jwks.json

--- src/main/java/me/hchome/demouser/controller/ClientController.java
+++ src/main/java/me/hchome/demouser/controller/ClientController.java
@@ -1,23 +1,51 @@
 package me.hchome.demouser.controller;
 
+import lombok.Data;
 import me.hchome.demouser.data.Client;
 import me.hchome.demouser.data.service.ClientService;
 import me.hchome.demouser.data.service.UserService;
+import me.hchome.demouser.security.key.ClientUsersAuthenticationKeyGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping(path = "/clients")
+
 public class ClientController {
+
+    @Data
+    private static class JWKS {
+        private Collection<JWK> keys;
+    }
+
+    @Data
+    private static class JWK {
+        private String alg;
+        private String use;
+        private String e;
+        private String n;
+        private String kty;
+        private String kid;
+
+    }
 
     @Autowired
     private ClientService service;
 
+    @Autowired
     private UserService userService;
+
+    @Autowired
+    private ClientUsersAuthenticationKeyGenerator generator;
 
     @PostMapping
     public CompletableFuture<Client> registerClient(@RequestBody Client client) {
@@ -25,8 +53,26 @@ public class ClientController {
     }
 
     @GetMapping("/.well-known/jwks.json")
-    public CompletableFuture<Client> getClient(@AuthenticationPrincipal Authentication authentication) {
-        return service.loadClientByClientId(authentication.getName());
+    public CompletableFuture<JWKS> getClient(@AuthenticationPrincipal Authentication authentication) {
+        return service.loadClientByClientId(authentication.getName())
+                .thenApplyAsync(client -> {
+                    JWKS jwks = new JWKS();
+                    JWK jwk = new JWK();
+                    byte[] bytes = client.getPublicKey();
+                    try {
+                        RSAPublicKey publicKey = (RSAPublicKey) generator.getPublic(bytes);
+                        jwk.setAlg(publicKey.getAlgorithm());
+                        jwk.setE(publicKey.getPublicExponent().toString(16));
+                        jwk.setN(publicKey.getModulus().toString(16));
+                        jwk.setUse("sig");
+                        jwk.setKty(generator.getAlgorithm());
+                        jwk.setKid(String.valueOf(client.getId()));
+                        jwks.keys = Collections.singleton(jwk);
+                        return jwks;
+                    } catch (InvalidKeySpecException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                });
     }
 
     @PatchMapping(params = "uid")
--- src/main/java/me/hchome/demouser/security/key/ClientUsersAuthenticationKeyGenerator.java
+++ src/main/java/me/hchome/demouser/security/key/ClientUsersAuthenticationKeyGenerator.java
@@ -1,22 +1,28 @@
 package me.hchome.demouser.security.key;
 
+import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
 import org.springframework.beans.factory.InitializingBean;
 
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
 
 public final class ClientUsersAuthenticationKeyGenerator implements InitializingBean {
 
     public KeyPair generateKeyPair() {
-
         return generator.generateKeyPair();
     }
 
+    public PublicKey getPublic(byte[] bytes) throws InvalidKeySpecException {
+        return factory.generatePublic(new X509EncodedKeySpec(bytes));
+    }
+
     private void initialize() throws NoSuchAlgorithmException {
-        generator = KeyPairGenerator.getInstance(DEFAULT_ALGORITHM);
+        generator = KeyPairGenerator.getInstance(algorithm);
+        generator.initialize(keySize);
+        factory = KeyFactory.getInstance(algorithm);
     }
 
     @Override
@@ -24,12 +30,15 @@ public final class ClientUsersAuthenticationKeyGenerator implements Initializing
         initialize();
     }
 
+    @Getter
     @Setter
     @NonNull
     private String algorithm = DEFAULT_ALGORITHM;
     @Setter
     private int keySize = DEFAULT_KEY_SIZE;
     private KeyPairGenerator generator;
+
+    private KeyFactory factory;
 
 
     private final static String DEFAULT_ALGORITHM = "RSA";
